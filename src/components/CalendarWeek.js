d61262e772e5e329f4a261cafc30e818186f4ef2
Renji <lprenji94@gmail.com>
2022-06-18 12:11:00 +0200

    fixed calendar time header

--- src/components/CalendarWeek.js
+++ src/components/CalendarWeek.js
@@ -2,8 +2,8 @@ import React from "react";
 import CalendarDay from "./CalendarDay.js";
 
 const CalendarWeek = ({weekDays = [], onClickFree, onClickHour}) => {
-  return <div className="w-full h-full relative">
-    <div className="w-full h-full flex absolute overflow-y-auto">
+  return <div className="w-full h-full relative overflow-y-auto">
+    <div className="w-full h-auto flex absolute">
       {weekDays.map((day, index) => {
         let key = day.date || Math.floor( Math.random() * 10000 )
         return <CalendarDay onClickFree={onClickFree} onClickHour={onClickHour} first={index==0} last={index == weekDays.length -1} key={key} day={day}/>
