4c9bd41cb58a10e60de83ff9f7e4c28dd36cc3f3
Renji <lprenji94@gmail.com>
2022-07-07 21:02:51 +0200

    updated gunHelper again

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import "./loadGun";
+
 import gunRules from './gunRules';
 
 import gunHelper from 'gunhelper';
