b5a0364cab2ca2c56e98ab3826a4d663ed1eb2e8
Renji <lprenji94@gmail.com>
2022-07-07 20:59:25 +0200

    loadGun

---
+++ src/loadGun.js
@@ -0,0 +1,10 @@
+import GUN from "gun/gun.js"
+import SEA from "gun/sea"
+import "gun/lib/yson.js";
+import "gun/lib/dom.js"
+import "gun/lib/upload.js"
+import "gun/lib/load.js";
+import "gun/lib/radix"
+import "gun/lib/radisk"
+import "gun/lib/store"
+import "gun/lib/rindexed"
