e21b8f719cfe0b8608ef2f627095da51979d6a17
Renji <lprenji94@gmail.com>
2022-09-30 10:29:29 +0200

    updated gunHelper

--- src/loadGun.js
+++ src/loadGun.js
@@ -1,4 +1,5 @@
-import GUN from "gun/gun.js"
+// import "gun/lib/mobile.js" // most important!
+import GUN from "gun/gun"
 import SEA from "gun/sea"
 import "gun/lib/yson.js";
 import "gun/lib/dom.js"
@@ -8,3 +9,8 @@ import "gun/lib/radix"
 import "gun/lib/radisk"
 import "gun/lib/store"
 import "gun/lib/rindexed"
+
+// import AsyncStorage from "@react-native-community/async-storage"
+// import asyncStore from 'gun/lib/ras.js'
+// Gun({ store: asyncStore({ AsyncStorage })})
+// Gun({})
